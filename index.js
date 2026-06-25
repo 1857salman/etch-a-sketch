585dd72b9307f6619acce7b29c58f8fcf01597ae
Salman2004 <137329744+1857salman@users.noreply.github.com>
2026-06-25 11:59:32 +0530

    Add hovering effect with increasing opacity.
    Hoveing effect included using event delegation
    method. On each visit, grid box opacity increases.
    The color of the box/grid is fixed so far.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ console.log(gridSize);
 console.log(typeof gridSize);
 
 const container = document.querySelector(".container");
-console.log(container);
 
 const boxWidth = 400 / gridSize;
 
@@ -13,3 +12,30 @@ for (let i = 0; i < gridSize * gridSize; i++) {
   box.style.width = `${boxWidth}px`;
   container.appendChild(box);
 }
+
+// code for pixelated trail that is etching!
+// Event delegation to access the grid elements or boxes inside the container div
+
+container.addEventListener("mouseover", (event) => {
+  const item = event.target.closest(".box");
+  if (!item) return; // Exit if the cursor isn't over a grid item
+
+  item.style.backgroundColor = "#ff0055";
+
+  let opacity = Number(item.style.opacity);
+  // console.log(typeof opacity);
+  if (opacity <= 1) {
+    opacity += 0.1;
+    item.style.opacity = `${opacity}`;
+  }
+  console.log(item.style.opacity);
+  console.log(typeof item.style.opacity);
+});
+
+container.addEventListener("mouseout", (event) => {
+  const item = event.target.closest(".box");
+
+  if (!item) return;
+
+  // item.style.backgroundColor = "initial";
+});

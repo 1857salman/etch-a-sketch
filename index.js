16de5cc147cfa250cd768bc84176099fc00dc499
Salman2004 <137329744+1857salman@users.noreply.github.com>
2026-06-26 23:54:10 +0530

    Add button to get grid size and set the grid.
    with the help of helper fuctions, button receives
    the gridsize, removes the prevous grid and sets
    the new grid.

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-let gridSize = Number(prompt("Enter the grid size:"));
-console.log(gridSize);
-console.log(typeof gridSize);
+// Set  the sketch pad width here: in px units
+const SKETCH_WIDTH = 420;
 
 const container = document.querySelector(".container");
+const button = document.querySelector(".btn");
 
-const boxWidth = 400 / gridSize;
+// sets conatiner height and width: sketch-pad size in px
+container.style.width = `${SKETCH_WIDTH}px`;
+container.style.height = `${SKETCH_WIDTH}px`;
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  let box = document.createElement("div");
-  box.classList.add("box");
-  box.style.width = `${boxWidth}px`;
-  container.appendChild(box);
+button.addEventListener("click", setPad);
+
+function setPad() {
+  const gridSize = Number(prompt("Enter the grid size:"));
+  // sets the size of each grid box/cell
+  const boxWidth = SKETCH_WIDTH / gridSize;
+
+  removeGrid();
+  setGrid(gridSize, boxWidth);
+
+  console.log(SKETCH_WIDTH);
+}
+//adapted from MDN
+function removeGrid() {
+  const container = document.querySelector(".container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function setGrid(gridSize, boxWidth) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    box.style.width = `${boxWidth}px`;
+    container.appendChild(box);
+  }
 }
 
 // code for pixelated trail that is etching!
@@ -20,22 +44,32 @@ container.addEventListener("mouseover", (event) => {
   const item = event.target.closest(".box");
   if (!item) return; // Exit if the cursor isn't over a grid item
 
-  item.style.backgroundColor = "#ff0055";
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
 
-  let opacity = Number(item.style.opacity);
-  // console.log(typeof opacity);
-  if (opacity <= 1) {
-    opacity += 0.1;
+  item.style.backgroundColor = rndCol;
+
+  let opacity = item.style.opacity ? Number(item.style.opacity) : 0.5;
+
+  if (opacity < 1) {
+    opacity += 0.05;
     item.style.opacity = `${opacity}`;
   }
-  console.log(item.style.opacity);
-  console.log(typeof item.style.opacity);
+  // console.log(item.style.opacity);
 });
 
 container.addEventListener("mouseout", (event) => {
   const item = event.target.closest(".box");
-
   if (!item) return;
-
-  // item.style.backgroundColor = "initial";
 });
+
+// Followin random color code is adapted from MDN
+// event object reading
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+// This function is not being used directly.
+function bgChange(e) {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  e.target.style.backgroundColor = rndCol;
+  console.log(e);
+}
